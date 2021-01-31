fb7295ffb883e07518a7dfd6a2bb757cff9b23f8
Diego Rivera <rivmont.diego@gmail.com>
2021-01-31 00:05:08 -0400

    Update Main.java
    Add my greetings to the code

--- src/lodge/Main.java
+++ src/lodge/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Dustin");
+        System.out.println("Hey Dustin, hope you are doing great");
     }
 
 }
